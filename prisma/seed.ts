60a47561083cc2e7b0449a6f180bf3d11b2e6f1c
Daniel Noworyta <daniel.noworyta@gmail.com>
2023-09-18 07:00:03 +0200

    Add prisma and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,17 @@
 // prisma/seed.ts
 import { PrismaClient } from '@prisma/client';
 import { Product } from 'src/products/entities/product.entity/product.entity';
+import { faker } from '@faker-js/faker';
 import * as dotenv from 'dotenv';
 
 const prisma = new PrismaClient();
 
 const fakeProduct = (): Product => ({
-  id: '1',
-  name: '',
-  description: '',
-  price: 1,
-  image: '',
+  id: faker.string.uuid(),
+  name: faker.commerce.productName(),
+  description: faker.commerce.productDescription(),
+  price: parseInt(faker.commerce.price({ min: 100, dec: 0 })),
+  image: faker.image.urlLoremFlickr({ category: 'fashion' }),
 });
 
 async function main() {
